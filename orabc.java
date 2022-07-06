9292ff87249e5d5913e691d829e792701d2200ff
LYSF <84968920+useraccount111@users.noreply.github.com>
2022-07-06 10:30:36 +0100

    Create orabc.java

---
+++ orabc.java
@@ -0,0 +1,12 @@
+public class orabc {
+  public static void main(String[] args) {
+      boolean finished = false
+      
+      if (finished == false) {
+          System.out.println("not done quite yet");
+      } else {
+          System.out.println("wip, this is yet to be finished");
+      }
+      
+  }
+}
